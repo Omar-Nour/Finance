b09cce5092f697146a71eb7ffd953c97cfa1c58f
Omar-Nour <omarnour21on@gmail.com>
2020-08-03 18:00:15 +0000

    added Procfile

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ Session(app)
 db = SQL("postgres://ihxilvkhipakrg:11e3ca314cbb0afa4f0d7a16676010c4d346a8bbda8744a1fe598eb7b133f844@ec2-54-246-87-132.eu-west-1.compute.amazonaws.com:5432/detjh626dgckoo")
 
 # Make sure API key is set
-if not os.environ.get("API_KEY"):
+if not api_key:
     raise RuntimeError("API_KEY not set")
 
 
